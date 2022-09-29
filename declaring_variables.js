53a09b85c367b7168b5ba60637de5024e7f11ba7
sheerazali166 <sheerazali656@gmail.com>
2022-09-29 13:29:04 +0500

    Kinza Sheikh Chocolaty

---
+++ declaring_variables.js
@@ -0,0 +1,67 @@
+
+
+var petDog = "Rex";
+
+var petcat = "Pepper";
+
+var petDog = "Rex";
+
+console.log(petDog); // Task 1 solution
+
+
+var petCat = "Pepper";
+
+console.log(petCat); // Task 2 solution
+
+
+var petDog = "Rex";
+
+console.log("My pet dog's name is:", petDog);
+
+
+var petCat = "Pepper"; // Task 2 solution
+
+console.log("My pet cat's name is:", petCat);
+
+
+var catSound = "purr";
+
+var dogSound= "woof";
+
+
+
+var petDog = "Rex"; 
+
+var dogSound = "woof"; // Task 8 solution
+
+console.log(petDog, "says", dogSound);
+
+
+
+var petCat = "Pepper"; // Task 2 solution
+
+var catSound = "purr"; // Task 7 solution
+
+
+console.log(petCat, "says", dogSound);
+
+
+
+var catSound = "purr"; // Task 7 solution
+
+catSound = "meow";
+
+
+var petCat = "Pepper"; // Task 2 solution
+
+var catSound = "meow"; // Task 11 solution
+
+
+console.log(petCat, "noew says", catSound);
+
+
+
+
+
+
+
